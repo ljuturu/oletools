2b83c0a0e8dc75f962a7750b8b21884781d7d618
sindhuri.juturu <sindhuri.juturu@fireeye.com>
2020-06-29 23:02:55 -0600

    Added vba stomping detection for all file types except slk, and text.

--- oletools/olevba.py
+++ oletools/olevba.py
@@ -3815,11 +3815,6 @@ class VBA_Parser(object):
         if self.type in (TYPE_SLK, TYPE_TEXT):
             self.vba_stomping_detected = False
             return False
-        # TODO: Files in memory cannot be analysed with pcodedmp yet
-        if not self.file_on_disk:
-            log.warning('For now, VBA stomping cannot be detected for files in memory')
-            self.vba_stomping_detected = False
-            return False
         # only run it once:
         if self.vba_stomping_detected is None:
             log.debug('Analysing the P-code to detect VBA stomping')
